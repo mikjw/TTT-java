c83ebe9915d921412b31ffb8381ef9c4026367fc
Mike <mjwalshe@gmail.com>
2019-11-15 20:05:10 +0000

    Set up JUnit 5

---
+++ test/Game.java
@@ -0,0 +1,12 @@
+public class Game {
+
+    private String name;
+
+    public Game(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ test/GameTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void getName() {
+        Game game = new Game("gamey");
+        assertEquals("gamey", game.getName());
+    }
+}
