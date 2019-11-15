15932faf32732b3c359a35e50d335316422b6a4b
Mike <mjwalshe@gmail.com>
2019-11-15 20:31:03 +0000

    Set up board as array with getter

--- test/Game.java
+++ test/Game.java
@@ -2,11 +2,17 @@ public class Game {
 
     private String name;
 
+    private char[] board = {'-','-','-'};
+
     public Game(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public char[] getBoard() {
+        return board;
     }
 }
--- test/GameTest.java
+++ test/GameTest.java
@@ -2,11 +2,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class GameTest {
 
     @Test
     void getName() {
         Game game = new Game("gamey");
         assertEquals("gamey", game.getName());
+    }
+
+    @Test
+    void getBoard() {
+        Game game = new Game("gamey");
+        assertArrayEquals(new char[] {'-','-','-'}, game.getBoard());
+
     }
 }
