4dba3ebf13baf938add8140b96c07bc229420d57
Mike <mjwalshe@gmail.com>
2019-11-18 19:48:07 +0000

    Move board assignment to field declaration

--- test/Game.java
+++ test/Game.java
@@ -1,23 +1,17 @@
 public class Game {
 
-    private char[][] board;
-
-    public boolean isPlayer_one_turn() {
-        return player_one_turn;
-    }
-
+    private char[][] board = new char[][] {{'-','-','-'}, {'-','-','-'}, {'-','-','-'}};
     private boolean player_one_turn = true;
-
-    public Game() {
-        this.board = new char[][] {{'-','-','-'}, {'-','-','-'}, {'-','-','-'}};
-    }
 
     public char[][] getBoard() {
         return board;
     }
 
-    public void move(int row, int col) {
+    public boolean isPlayer_one_turn() {
+        return player_one_turn;
+    }
 
+    public void move(int row, int col) {
         this.board[row][col] = 'x';
     }
 }
