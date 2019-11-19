6d635b1ffb70ca6486b31d1f9f3ad06f7366f5d4
Mike <mjwalshe@gmail.com>
2019-11-19 14:53:38 +0000

    Pass test for full board

--- test/Game.java
+++ test/Game.java
@@ -2,13 +2,21 @@ public class Game {
 
     private char[][] board = new char[][] {{'-','-','-'}, {'-','-','-'}, {'-','-','-'}};
     private boolean playerOneTurn = true;
+    private boolean boardFull = true;
 
     public char[][] getBoard() {
+        System.out.print(board[0]);
+        System.out.print(board[1]);
+        System.out.print(board[2]);
         return board;
     }
 
     public boolean isplayerOneTurn() {
         return playerOneTurn;
+    }
+
+    public boolean isBoardFull() {
+        return boardFull;
     }
 
     public void move(int row, int col) {
--- test/GameTest.java
+++ test/GameTest.java
@@ -55,6 +55,22 @@ class GameTest {
         game.move(2, 2);
         assertArrayEquals(new char[][] {{'x','-','-'}, {'-','-','-'}, {'-','-','o'}}, game.getBoard());
     }
+
+    @Test
+    void checkBoardFull() {
+        Game game = new Game();
+        game.move(1, 1);
+        game.move(2, 1);
+        game.move(2, 2);
+        game.move(1, 2);
+        game.move(0, 2);
+        game.move(2, 0);
+        game.move(0, 0);
+        game.move(0, 1);
+        game.move(1, 0);
+        assertArrayEquals(new char[][] {{'x','o','x'}, {'x','x','o'}, {'o','o','x'}}, game.getBoard());
+        assertTrue(game.isBoardFull());
+    }
 }
 
 
