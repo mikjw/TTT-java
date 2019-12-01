591411db58d6e08afea94027c4c34dcf58f50e6c
Mike <mjwalshe@gmail.com>
2019-12-01 18:33:36 +0000

    Add basic user input

--- src/Interface.java
+++ src/Interface.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Interface {
 
     public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
         Game game = new Game();
-        System.out.println(game.getBoard());
+        while (true) {
+            System.out.println("Enter row:");
+            int row = scanner.nextInt();
+            System.out.println("Enter col:");
+            int col = scanner.nextInt();
+            game.move(row, col);
+            System.out.println(game.getBoard());
+        }
     }
 }
