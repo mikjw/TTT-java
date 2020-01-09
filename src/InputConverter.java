39df877c552f5779d035c35e45e0b93d5125c786
Mike <mjwalshe@gmail.com>
2020-01-09 11:15:02 +0000

    Refactor to use hashtable

--- src/InputConverter.java
+++ src/InputConverter.java
@@ -1,11 +1,15 @@
+import java.util.Hashtable;
+
 public class InputConverter {
     public int[] convert(String input) {
-        int[] output;
-        if (input == "B2") {
-            output = new int[]{1, 1};
-        } else if (input == "A2") {
-            output = new int[]{0, 1};
-        } else   output = new int[]{2, 2};
+        String[] arr = input.split("");
+        Hashtable<String, Integer> conversion = new Hashtable<>();
+        conversion.put("A", 0);
+        conversion.put("B", 1);
+        conversion.put("C", 2);
+        int[] output = new int[2];
+        output[0] = conversion.get(arr[0]);
+        output[1] = Integer.parseInt(arr[1]) - 1;
         return output;
     }
 }
